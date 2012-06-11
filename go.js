877e2b4819c0d819d7b8049a295b833fcd0b0648
Kelley van Evert <kelleyvanevert@gmail.com>
2012-06-12 00:10:28 +0200

    a bit simpler code, but performance went DOWN ;)

--- go.js
+++ go.js
@@ -1,4 +1,10 @@
 
+// Utils
+var withinBoard = function (v) {
+  return 0 <= v.x && v.x <= 18 && 0 <= v.y && v.y <= 18;
+};
+
+// Immutable
 var Vec = function (x, y) {
       this.x = x;
       this.y = y;
@@ -39,247 +45,162 @@ _.extend(Vec.prototype, {
   }
 });
 
-var Cell = function (pos) {
-      this.pos = pos;
-      this.color = ko.observable(false);
-      this.empty = ko.computed(function () {
-        return !this.color();
-      }, this);
-    };
-
-var Board = function () {
-      this.cells = [];
+// Immutable
+var State = function () {
+      //  0: none
+      //  1: white
+      // -1: black
+      this.cells = {};
       for (var y = 0; y < 19; y++) {
-        this.cells[y] = [];
         for (var x = 0; x < 19; x++) {
-          this.cells[y][x] = new Cell(new Vec(x, y));
+          this.cells[new Vec(x, y)] = 0;
         }
       }
       
-      this.highlight = ko.observable(false);
+      this.highlight = false;
     };
 
-Board.restrict = function (vs) {
-  return _.filter(vs, function (v) {
-    return 0 <= v.x && v.x <= 18 && 0 <= v.y && v.y <= 18;
-  });
-};
-
-_.extend(Board.prototype, {
-  get: function (v) {
-    return this.cells[v.y][v.x];
+_.extend(State.prototype, {
+  clone: function () {
+    var s = new State();
+    s.cells = _.clone(this.cells);
+    s.highlight = this.highlight;
+    return s;
+  },
+  stoneAt: function (v) {
+    var i = this.cells[v];
+    if (i === 0) return false;
+    if (i === 1) return "white";
+    return "black";
   },
   putStoneAt: function (v, color) {
-    this.get(v).color(color);
+    var s = this.clone();
+    s.cells[v] = ["black", "and", "white"].indexOf(color) - 1;
+    return s;
   },
   removeStoneAt: function (v) {
-    this.get(v).color(false);
+    var s = this.clone();
+    s.cells[v] = 0;
+    return s;
   },
-  getLayoutObject: function () {
-    var layout = {};
-    _.map(this.cells, function (row) {
-      _.map(row, function (cell) {
-        if (!cell.empty())
-          layout[cell.pos.toString()] = cell.color();
-      });
-    });
-    return layout;
-  },
-  stellation: function (v) {
-    if (this.get(v).empty())
-      return [];
-    
-    var stellation = {},
+  // discoverChain : Vec -> [Vec]
+  discoverChain: function (v) {
+    var cells = this.cells,
+        color = cells[v],
+        chain = {},
         todo = [v];
     
-    while (todo.length > 1) {
-      var v = todo.pop(),
-          nei = Board.restrict(v.neighbours());
-      for (var i = 0, n; (i < nei.length) && (n = nei[i]); i++) {
-        if (!stellation[n.toString()] && this.get(n).color === this.get(v).color)
+    if (color === 0)
+      throw "[discoverChain] Given vector must refer to a stone";
+    
+    while (todo.length > 0) {
+      var v = todo.pop();
+      chain[v] = v;
+      _(v.neighbours()).chain().filter(withinBoard).forEach(function (n) {
+        if (cells[n] === color && !chain[v])
           todo.push(n);
-      }
-      stellation[v.toString()] = this.get(v);
+      });
     }
     
-    return _.toArray(stellation);
+    return _.toArray(chain);
   },
-  liberties: function (cells) {
-    var self = this,
-        liberties = _.reduce(cells, function (liberties, cell) {
-          var lib = _.filter(Board.restrict(cell.pos.neighbours()), function (v) {
-            return !liberties[v.toString()] && self.get(v).empty();
-          });
-          _.map(lib, function (v) {
-            liberties[v.toString()] = self.get(v);
-          });
-          return liberties;
-        }, {});
+  // alive: [Vec] -> Boolean
+  alive: function (chain) {
+    if (chain.length === 0)
+      throw "[alive] Given chain must be non-empty";
     
-    return _.toArray(liberties);
-  }
-});
-
-var History = function (board) {
-      this.board = board;
-      this.deltas = [];
-      this.at = ko.observable(0);
-    };
-
-_.extend(History.prototype, {
-  write: function (delta) {
-    this.deltas.splice(this.at(), this.deltas.length - this.at(), delta);
-    this.forward();
-    return this;
-  },
-  backward: function () {
-    var self = this,
-        board = this.board,
-        delta;
+    var cells = this.cells;
     
-    if (this.at() > 0) {
-      this.at(this.at() - 1);
-      delta = this.deltas[this.at()];
-      _.map(delta.added || {}, function (color, v) {
-        board.removeStoneAt(Vec.parse(v));
-      });
-      _.map(delta.removed || {}, function (color, v) {
-        board.putStoneAt(Vec.parse(v), color);
-      });
-      if (this.at() === 0) {
-        board.highlight(false);
-      } else {
-        board.highlight(this.deltas[this.at() - 1].highlight);
-      }
-    }
-    return this;
-  },
-  forward: function () {
-    var self = this,
-        board = this.board,
-        delta;
-    
-    if (this.at() < this.deltas.length) {
-      delta = this.deltas[this.at()];
-      this.at(this.at() + 1);
-      _.map(delta.added || {}, function (color, v) {
-        board.putStoneAt(Vec.parse(v), color);
-      });
-      _.map(delta.removed || {}, function (color, v) {
-        board.removeStoneAt(Vec.parse(v));
-      });
-      board.highlight(delta.highlight);
-    }
-    return this;
-  },
-  rewind: function () {
-    while (this.at() > 0) {
-      this.backward();
-    }
-    return this;
-  },
-  fastForward: function () {
-    while (this.at() < this.deltas.length) {
-      this.forward();
-    }
-    return this;
+    return _(chain).chain().map(function (v) {
+      return v.neighbours();
+    }).flatten().filter(withinBoard).any(function (v) {
+      return cells[v] === 0;
+    });
   }
 });
 
 var Game = function () {
-      var self = this,
-          board = this.board = new Board(),
-          history = this.history = new History(this.board);
+      var self    = this,
+          history = this.history = ko.observableArray([ new State() ]),
+          at      = this.at      = ko.observable(0),
+          state   = this.state   = ko.computed(function () { return history()[ at() ]; }),
+          turn    = this.turn    = ko.computed(function () { return ["white", "black"][ at() % 2 ]; }),
+          stones  = this.stones  = [];
       
-      this.turn = ko.computed(function () {
-        return ["white", "black"][this.at() % 2];
-      }, history);
+      _.map(ko.utils.range(0, 18), function (y) {
+        stones[y] = [];
+        _.map(ko.utils.range(0, 18), function (x) {
+          stones[y][x] = {
+            x: x,
+            y: y,
+            color: ko.computed(function () {
+              return state().stoneAt(new Vec(x, y));
+            })
+          };
+        });
+      });
       
-      this.place = function (cell) {
-        var color = self.turn(),
-            othercolor = ["black", "white"][["white", "black"].indexOf(color)],
-            vec = cell.pos.clone(),
-            add = {},
-            remove = {},
-            layout = board.getLayoutObject();
-        
-        if (layout.hasOwnProperty(vec.toString()))
-          throw "Weird Error";
-        
-        // Add this new stone
-        add[vec.toString()] = color;
-        layout[vec.toString()] = color;
-        
-        // Remove surrounded chains of other player
-        _.map(Board.restrict(vec.neighbours()), function (n) {
-          if (!layout.hasOwnProperty(n.toString()) || layout[n.toString()] == color)
-            return;
-          
-          var chain = Game.chain(layout, n);
-          if (Game.surrounded(layout, chain, othercolor)) {
-            _.map(chain, function (v) {
-              remove[v.toString()] = othercolor;
-              delete layout[v.toString()];
+      // Operations
+      var backward = this.backward = function () {
+            if (at() > 0)
+              at(at() - 1);
+            return this;
+          },
+          forward = this.forward = function () {
+            if (at() < history().length)
+              at(at() + 1);
+            return this;
+          },
+          rewind = this.rewind = function () {
+            while (at() > 0)
+              backward();
+            return this;
+          },
+          fastForward = this.fastForward = function () {
+            while (at() < history().length)
+              forward();
+            return this;
+          },
+          place = this.place = function (cell) {
+            var v = new Vec(cell.x, cell.y),
+                color = turn(),
+                othercolor = (color === "black" ? "white" : "black"),
+                newstate = state().putStoneAt(v, color);
+            
+            // Remove surrounded neighbouring chains of other player
+            _(v.neighbours()).chain().filter(withinBoard).forEach(function (n) {
+              if (newstate.stoneAt(n) !== othercolor) return;
+              console.log("maybe remove noughbouring chain of other player...");
+              var chain = newstate.discoverChain(n);
+              if (!newstate.alive(chain)) {
+                console.log("remove!");
+                _.map(chain, function (w) {
+                  newstate = newstate.removeStoneAt(w);
+                });
+              }
             });
-          }
-        });
-        
-        // Possibly die
-        var chain = Game.chain(layout, vec);
-        if (Game.surrounded(layout, chain, color)) {
-          _.map(chain, function (v) {
-            remove[v.toString()] = layout[v.toString()];
-            delete layout[v.toString()];
-          });
-          delete add[vec.toString()];
-          delete remove[vec.toString()];
-        }
-        
-        history.write({
-          added: add,
-          removed: remove,
-          highlight: vec
-        });
-      };
+            
+            // Possibly die
+            var chain = newstate.discoverChain(v);
+            if (!newstate.alive(chain)) {
+              _.map(chain, function (w) {
+                newstate = newstate.removeStoneAt(w);
+              });
+            }
+            
+            history.push(newstate);
+            at(at() + 1);
+            return this;
+          };
     };
-
-_.extend(Game, {
-  chain: function (layout, init) {
-    var chain = [],
-        color = layout[init.toString()],
-        done = {},
-        todo = [init];
-    
-    while (todo.length > 0) {
-      var v = todo.pop();
-      chain.push(v);
-      done[v.toString()] = true;
-      _.forEach(Board.restrict(v.neighbours()), function (n) {
-        if (done[n.toString()] || layout[n.toString()] != color) return;
-        todo.push(n);
-      });
-    }
-    
-    return chain;
-  },
-  surrounded: function (layout, vs) {
-    var color = layout[vs[0].toString()];
-    
-    return _.any(vs, function (v) {
-      return _.any(Board.restrict(v.neighbours()), function (n) {
-        return !layout.hasOwnProperty(n.toString());
-      });
-    });
-  }
-});
 
 var game = new Game();
 
 var keyDownActions = {
-  "37": _.bind(game.history.backward,    game.history), // left
-  "39": _.bind(game.history.forward,     game.history), // right
-  "38": _.bind(game.history.rewind,      game.history), // up
-  "40": _.bind(game.history.fastForward, game.history)  // down
+  "37": function () { game.backward()    }, // left
+  "39": function () { game.forward()     }, // right
+  "38": function () { game.rewind()      }, // up
+  "40": function () { game.fastForward() }  // down
 };
 
 $(function () {

6b09e686623fcf41883c41f0cf6414e00bb76947
Kelley van Evert <kelleyvanevert@gmail.com>
2012-12-06 01:53:38 +0100

    got a simple, but very intolerant, sgf game load function up and running

--- ui.js
+++ ui.js
@@ -1,17 +1,60 @@
 
-var game = new jsgo.Game();
+/*
+  This loadSGF is not very tolerant,
+    will very likely throw exceptions!!
+*/
+var loadSGFVariation = function (sgf, game) {
+      // place moves
+      for (var i = 0; i < sgf.sequence.nodes.length; i++) {
+        var move_prop = sgf.sequence.nodes[i].properties[0],
+            color = move_prop.identifier;
 
-var keyDownActions = {
-  "40": function () { game.backward()    }, // down
-  "38": function () { game.forward()     }, // up
-  "37": function () { game.backward()    }, // left
-  "39": function () { game.forward()     }, // right
-  "36": function () { game.rewind()      }, // home
-  "35": function () { game.fastForward() }, // end
-  "9": function (e) {
-    var n = game.history().rotateActiveChild(e.shiftKey ? -1 : 1);
-  } // tab
-};
+        if ("WB".split("").indexOf(color) < 0)
+          throw "[loadSGFVariation] move property identifier not W, B or C";
+
+        if (color.toLowerCase() != game.turn()[0].toLowerCase())
+          throw "[loadSGFVariation] white/black turn error";
+
+        var coord_str = move_prop.values[0].value.match(/^([a-s])([a-s])$/),
+            vec = new jsgo.Vec(coord_str[1].charCodeAt(0) - 97, coord_str[2].charCodeAt(0) - 97);
+
+        game.place(vec);
+      }
+      var nMovesPlayed = i;
+
+      // recurse for variation
+      for (var i = 0; i < sgf.gametrees.length; i++) {
+        loadSGFVariation(sgf.gametrees[i], game);
+      }
+
+      // rewind this particular variation tree
+      for (var i = 0; i < nMovesPlayed; i++)
+        game.backward();
+
+      return game;
+    },
+    SGFtoGame = function (sgf_str) {
+      try {
+        var sgf = parseSGF(sgf_str);
+        return loadSGFVariation(sgf.gametrees[0], new jsgo.Game());
+      } catch (e) {
+        return false;
+      }
+    },
+    UI = {
+      game: ko.observable(new jsgo.Game())
+    },
+    keyDownActions = {
+      "40": function () { UI.game().backward()    }, // down
+      "38": function () { UI.game().forward()     }, // up
+      "37": function () { UI.game().backward()    }, // left
+      "39": function () { UI.game().forward()     }, // right
+      "36": function () { UI.game().rewind()      }, // home
+      "35": function () { UI.game().fastForward() }, // end
+      "9": function (e) {
+        var n = UI.game().history().rotateActiveChild(e.shiftKey ? -1 : 1);
+      } // tab
+    };
 
 $(function () {
   $(window).keydown(function (e) {
@@ -21,5 +64,5 @@ $(function () {
       return false;
     }
   });
-  ko.applyBindings(game, $("#html")[0]);
+  ko.applyBindings(UI, $("#html")[0]);
 });
